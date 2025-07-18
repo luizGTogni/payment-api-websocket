25e2355a9e7cf8a132a640ac86b22d5e2f866980
Luiz Togni <contato.togni@gmail.com>
2025-07-18 20:27:20 -0300

    feat: add flask initial structure

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+from os import getenv
+
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("SQLALCHEMY_DATABASE_URI")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = getenv("FLASK_SECRET_KEY")
+
+db = SQLAlchemy(app)
