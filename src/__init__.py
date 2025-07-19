37ad120e47a1c52b77366d74ec0ce4de8fb3f931
Luiz Togni <contato.togni@gmail.com>
2025-07-19 00:21:27 -0300

    feat: import routes and socket

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,7 @@ from os import getenv
 from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
 
 load_dotenv()
 
@@ -12,5 +13,12 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = getenv("FLASK_SECRET_KEY")
 
 db = SQLAlchemy(app)
+socketio = SocketIO(app)
 
-from src.routes import create_payment_pix, get_qrcode_pix, payment_pix_page
+from src.routes import (
+    create_payment_pix,
+    get_qrcode_pix,
+    payment_pix_page,
+    pix_confirmation,
+)
+from src.socket import connect
