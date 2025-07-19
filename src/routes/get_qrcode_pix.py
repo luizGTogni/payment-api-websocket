15cc2fee44f213919a313351926d3eed191c78c0
Luiz Togni <contato.togni@gmail.com>
2025-07-18 22:21:25 -0300

    refactor: change route added more path qr_code

--- src/routes/get_qrcode_pix.py
+++ src/routes/get_qrcode_pix.py
@@ -3,6 +3,6 @@ from flask import send_file
 from src import app
 
 
-@app.route("/payments/pix/<file_name>", methods=["GET"])
+@app.route("/payments/pix/qr_code/<file_name>", methods=["GET"])
 def get_qrcode_pix(file_name):
     return send_file(f"{os.getcwd()}/static/img/{file_name}.png", mimetype="image/png")
