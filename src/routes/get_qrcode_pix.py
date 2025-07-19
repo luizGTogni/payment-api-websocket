cb8d2f2ab68a497e4297bba143db55a58b93929a
Luiz Togni <contato.togni@gmail.com>
2025-07-18 22:10:14 -0300

    feat: add get qrcode pix image route

---
+++ src/routes/get_qrcode_pix.py
@@ -0,0 +1,8 @@
+import os
+from flask import send_file
+from src import app
+
+
+@app.route("/payments/pix/<file_name>", methods=["GET"])
+def get_qrcode_pix(file_name):
+    return send_file(f"{os.getcwd()}/static/img/{file_name}.png", mimetype="image/png")
