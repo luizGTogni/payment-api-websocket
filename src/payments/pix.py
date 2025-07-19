5fa1e6d6b3be3c192545ff5db518c30deb892283
Luiz Togni <contato.togni@gmail.com>
2025-07-19 00:23:04 -0300

    feat: add remove_qr_code method

--- src/payments/pix.py
+++ src/payments/pix.py
@@ -13,7 +13,13 @@ class Pix:
         hash_payment = f"{os.urandom(12).hex()}_{bank_payment_id}"
 
         img = qrcode.make(hash_payment)
-        img_name = f"qr_code_payment_{bank_payment_id}"
-        img.save(f"{os.getcwd()}/static/img/{img_name}.png")
+        file_name = f"qr_code_payment_{bank_payment_id}"
+        img.save(f"{os.getcwd()}/static/img/{file_name}.png")
 
-        return {"bank_payment_id": bank_payment_id, "qr_code_path": img_name}
+        return {"bank_payment_id": bank_payment_id, "qr_code_path": file_name}
+
+    def remove_qr_code(self, bank_payment_id):
+        if os.path.exists(
+            f"{os.getcwd()}/static/img/qr_code_payment_{bank_payment_id}.png"
+        ):
+            os.remove(f"{os.getcwd()}/static/img/qr_code_payment_{bank_payment_id}.png")
